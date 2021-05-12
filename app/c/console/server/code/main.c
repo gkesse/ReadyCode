72a44140b3c446b470383737de241db76722caa8
Gerard KESSE <tiakagerard@hotmail.com>
2021-05-12 10:00:05 +0200

    Initial Commit

--- app/c/console/server/code/main.c
+++ app/c/console/server/code/main.c
@@ -1,9 +1,6 @@
 //===============================================
 #include <gtk/gtk.h>
 //===============================================
-#define NUM_NAMES 4
-const gchar* names[] = { "Andrew", "Joe", "Samantha", "Jonathan" };
-//===============================================
 static void GWindow_OnDestroy(GtkWidget* obj, gpointer params);
 //===============================================
 int main(int argc, char** argv) {
@@ -14,16 +11,12 @@ int main(int argc, char** argv) {
     gtk_container_set_border_width(GTK_CONTAINER(lWindow), 0);
     gtk_widget_set_size_request(GTK_WIDGET(lWindow), 400, 200);
     
-    GtkWidget* vbox = gtk_box_new (GTK_ORIENTATION_VERTICAL, 5);
-
-    for (int i = 0; i < NUM_NAMES; i++) {
-        GtkWidget *button = gtk_button_new_with_label (names[i]);
-        gtk_box_pack_start(GTK_BOX (vbox), button, 1, 0, 0);
-
-        g_signal_connect_swapped (G_OBJECT (button), "clicked", G_CALLBACK (gtk_widget_destroy), (gpointer) button);
-    }
-
-    gtk_container_add(GTK_CONTAINER(lWindow), vbox);
+    GtkWidget* label = gtk_label_new ("Double-Click Me!");
+    
+    GtkWidget* eventbox = gtk_event_box_new ();
+    gtk_container_add (GTK_CONTAINER (eventbox), label);
+      
+    gtk_container_add(GTK_CONTAINER(lWindow), eventbox);
     gtk_widget_show_all(lWindow);
     
     g_signal_connect(G_OBJECT(lWindow), "destroy", G_CALLBACK(GWindow_OnDestroy), NULL);
