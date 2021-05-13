2ce3fa226b64cf6e308efe35e705b3a55295fdcc
Gerard KESSE <tiakagerard@hotmail.com>
2021-05-13 23:30:06 +0200

    Initial Commit

--- app/c/console/server/code/main.c
+++ app/c/console/server/code/main.c
@@ -1,31 +1,49 @@
 //===============================================
-#include <gtk/gtk.h>
+#include <stdio.h>
+#include <sqlite3.h>
 //===============================================
-static void GWindow_OnDestroy(GtkWidget* obj, gpointer params);
+static int onExec(void* params, int colCount, char** colValue, char** colName);
 //===============================================
 int main(int argc, char** argv) {
-    gtk_init(&argc, &argv);
-    
-    GtkWidget* lWindow = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(lWindow), "ReadyApp");
-    gtk_container_set_border_width(GTK_CONTAINER(lWindow), 0);
-    gtk_widget_set_size_request(GTK_WIDGET(lWindow), 400, 200);
-    
-    GtkWidget* label = gtk_label_new ("Double-Click Me!");
-    
-    GtkWidget* eventbox = gtk_event_box_new ();
-    gtk_container_add (GTK_CONTAINER (eventbox), label);
-      
-    gtk_container_add(GTK_CONTAINER(lWindow), eventbox);
-    gtk_widget_show_all(lWindow);
-    
-    g_signal_connect(G_OBJECT(lWindow), "destroy", G_CALLBACK(GWindow_OnDestroy), NULL);
-    
-    gtk_main();
+    sqlite3* lDb;
+    char* lError;
+    int lHeader = 1;
+    char lSql[256] = ""
+    "select * from config_data\n"
+    "";
+    sqlite3_open("database.dat", &lDb);
+    sqlite3_exec(lDb, lSql, onExec, &lHeader, &lError);
+    sqlite3_close(lDb);
     return 0;
 }
 //===============================================
-static void GWindow_OnDestroy(GtkWidget* obj, gpointer params) {
-    gtk_main_quit();
+static int onExec(void* params, int colCount, char** colValue, char** colName) {
+    int* lHeader = (int*)params;
+    // header
+    if((*lHeader) == 1) {
+        for(int i = 0; i < colCount; i++) {
+            const char* lName = colName[i];
+            if(i != 0) {printf(" | ");}
+            printf("%*s", -20, lName);
+        }
+        printf("\n");
+        // sep
+        for(int i = 0; i < colCount; i++) {
+            if(i != 0) {printf("-|-");}
+            for(int j = 0; j < 20; j++) {
+                printf("-");
+            }
+        }
+        (*lHeader) = 0;
+        printf("\n");
+    }
+    // data
+    for(int i = 0; i < colCount; i++) {
+        const char* lValue = colValue[i];
+        if(i != 0) {printf(" | ");}
+        printf("%*s", -20, lValue);
+    }
+    printf("\n");
+    return 0;
 }
 //===============================================
--- app/cpp/console/code/main.cpp
+++ app/cpp/console/code/main.cpp
@@ -2,7 +2,14 @@
 #include <iostream>
 //===============================================
 int main(int argc, char** argv) {
-    std::cout << "Bonjour tout le monde\n";
+    int lNumber = 3;
+    int i = 0;
+    for(;;) {
+        int lData = i*lNumber;
+        printf("%d x %d = %d\n", i, lNumber, lData);
+        i++;
+        if(i >= 10) {break;}
+    }
     return 0;
 }
 //===============================================
