2f118a2917a0142cd17ff8df643595bab083778b
Gerard KESSE <tiakagerard@hotmail.com>
2021-07-14 10:25:49 +0200

    Initial Commit

--- app/oracle_database/code/script.sql
+++ app/oracle_database/code/script.sql
@@ -2,14 +2,19 @@
 set serveroutput on;
 -- ==============================================
 declare
-    a integer := 22;
-    b integer := 7;
+    a boolean := true;
+    b boolean := false;
 -- ==============================================
 begin
-    dbms_output.put_line(a || ' + ' || b || ' = ' || (a + b));
-    dbms_output.put_line(a || ' - ' || b || ' = ' || (a - b));
-    dbms_output.put_line(a || ' * ' || b || ' = ' || (a * b));
-    dbms_output.put_line(a || ' / ' || b || ' = ' || (a / b));
+    dbms_output.put_line('!' || sys.diutil.bool_to_int(a) || ' = ' || sys.diutil.bool_to_int(not(a)));
+    dbms_output.put_line('!' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(not(b)));
+    dbms_output.put_line(sys.diutil.bool_to_int(a) || ' || ' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(a or b));
+    dbms_output.put_line(sys.diutil.bool_to_int(a) || ' || ' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(a or b));
+    dbms_output.put_line(sys.diutil.bool_to_int(b) || ' || ' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(b or b));
+    dbms_output.put_line(sys.diutil.bool_to_int(a) || ' || ' || sys.diutil.bool_to_int(a) || ' = ' || sys.diutil.bool_to_int(a or a));
+    dbms_output.put_line(sys.diutil.bool_to_int(a) || ' && ' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(a and b));
+    dbms_output.put_line(sys.diutil.bool_to_int(b) || ' && ' || sys.diutil.bool_to_int(b) || ' = ' || sys.diutil.bool_to_int(b and b));
+    dbms_output.put_line(sys.diutil.bool_to_int(a) || ' && ' || sys.diutil.bool_to_int(a) || ' = ' || sys.diutil.bool_to_int(a and a));
 end;
 /
 -- ==============================================
