f7130d3bf3b033e9ce33bf112d69a3a9af6d8bd5
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-05 15:36:26 +0200

    Initial Commit

--- app/cpp/cours/console/code/src/GTriangleIndex.h
+++ app/cpp/cours/console/code/src/GTriangleIndex.h
@@ -6,15 +6,31 @@
 //===============================================
 class GTriangleIndex : public GOpenGLUi {
 public:
-    GTriangleIndex();
-    virtual ~GTriangleIndex();
-    virtual void run(int argc, char** argv);
-    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-    static void processInput(GLFWwindow *window);
+	GTriangleIndex();
+	virtual ~GTriangleIndex();
+	virtual void run(int argc, char** argv);
+	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+	static void processInput(GLFWwindow *window);
 
 private:
-    const unsigned int SCR_WIDTH = 400;
-    const unsigned int SCR_HEIGHT = 400;
+	const unsigned int SCR_WIDTH = 400;
+	const unsigned int SCR_HEIGHT = 400;
+
+private:
+	const char *vertexShaderSource = ""
+			"#version 330 core\n"
+			"layout (location = 0) in vec3 aPos;\n"
+			"void main()\n"
+			"{\n"
+			"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+			"}\n";
+	const char *fragmentShaderSource = ""
+			"#version 330 core\n"
+			"out vec4 FragColor;\n"
+			"void main()\n"
+			"{\n"
+			"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+			"}\n";
 };
 //==============================================
 #endif
