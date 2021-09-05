91848313269534a7982efcc34f83c2324816ac51
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-05 16:06:56 +0200

    Initial Commit

--- app/cpp/cours/console/code/src/GTriangleDraw.cpp
+++ app/cpp/cours/console/code/src/GTriangleDraw.cpp
@@ -1,22 +1,6 @@
 //===============================================
 #include "GTriangleDraw.h"
 //===============================================
-const char *vertexShaderSource = ""
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-        "}\n";
-//===============================================
-const char *fragmentShaderSource = ""
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-        "}\n";
-//===============================================
 GTriangleDraw::GTriangleDraw() {
 
 }
@@ -86,7 +70,7 @@ void GTriangleDraw::run(int argc, char** argv) {
     glDeleteShader(fragmentShader);
 
     float vertices[] = {
-            -0.5f, -0.5f, 0.0f,    // left
+            -0.5f, -0.5f, 0.0f, // left
             0.5f, -0.5f, 0.0f,  // right
             0.0f,  0.5f, 0.0f   // top
     };
--- app/cpp/cours/console/code/src/GTriangleIndex.cpp
+++ app/cpp/cours/console/code/src/GTriangleIndex.cpp
@@ -1,5 +1,6 @@
 //===============================================
 #include "GTriangleIndex.h"
+#include "GManager.h"
 //===============================================
 GTriangleIndex::GTriangleIndex() {
 
@@ -33,41 +34,7 @@ void GTriangleIndex::run(int argc, char** argv) {
         return;
     }
 
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    unsigned int shaderProgram = GManager::Instance()->createProgram(vertexShaderSource, fragmentShaderSource);
 
     float vertices[] = {
             0.5f,  0.5f, 0.0f,  // top right
@@ -102,7 +69,7 @@ void GTriangleIndex::run(int argc, char** argv) {
         //===============================================
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
+        //===============================================
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
