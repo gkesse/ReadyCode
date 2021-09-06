59ed214de5ebd9387c34249f5fb4d2c9accf61ae
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-06 13:17:50 +0200

    Initial Commit

--- app/cpp/cours/console/code/src/GOpenGLUi.h
+++ app/cpp/cours/console/code/src/GOpenGLUi.h
@@ -6,33 +6,36 @@
 //===============================================
 class GOpenGLUi {
 public:
-	GOpenGLUi();
-	virtual ~GOpenGLUi();
-	static GOpenGLUi* Create(const std::string& key);
-	virtual void run(int argc, char** argv);
-	static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-	static void processInput(GLFWwindow *window);
+    GOpenGLUi();
+    virtual ~GOpenGLUi();
+    static GOpenGLUi* Create(const std::string& key);
+    virtual void run(int argc, char** argv);
+    static void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+    static void processInput(GLFWwindow *window);
 
 private:
-	const unsigned int SCR_WIDTH = 400;
-	const unsigned int SCR_HEIGHT = 400;
+    const unsigned int SCR_WIDTH = 400;
+    const unsigned int SCR_HEIGHT = 400;
 
 private:
-	const char *vertexShaderSource =""
-		"#version 330 core\n"
-	    "layout (location = 0) in vec3 aPos;\n"
-	    "void main()\n"
-	    "{\n"
-	    "   gl_Position = vec4(aPos, 1.0);\n"
-	    "}\0";
-	const char *fragmentShaderSource = ""
-		"#version 330 core\n"
-	    "out vec4 FragColor;\n"
-	    "uniform vec4 ourColor;\n"
-	    "void main()\n"
-	    "{\n"
-	    "   FragColor = ourColor;\n"
-	    "}\n\0";
+    const char *vertexShaderSource =""
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 1) in vec3 aColor;\n"
+        "out vec3 ourColor;\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = vec4(aPos, 1.0);\n"
+        "   ourColor = aColor;\n"
+        "}\0";
+    const char *fragmentShaderSource = ""
+        "#version 330 core\n"
+        "out vec4 FragColor;\n"
+        "in vec3 ourColor;\n"
+        "void main()\n"
+        "{\n"
+        "   FragColor = vec4(ourColor, 1.0f);\n"
+        "}\n\0";
 };
 //==============================================
 #endif
