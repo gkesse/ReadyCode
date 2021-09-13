2db9f0699c870a1b0126286d0345dad77b432ca0
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-13 23:02:13 +0200

    Initial Commit

--- app/cpp/cours/console/unix/data/shader/fragment_shader.fs
+++ app/cpp/cours/console/unix/data/shader/fragment_shader.fs
@@ -10,6 +10,6 @@ uniform sampler2D texture2;
 
 void main()
 {
-    FragColor = texture(texture2, TexCoord);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
 //===============================================
