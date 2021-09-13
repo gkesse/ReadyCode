ef447923a220672a4ebd1f314d4fe69131e398cd
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-13 22:25:58 +0200

    Initial Commit

--- app/cpp/cours/console/code/src/GOpenGLUi.h
+++ app/cpp/cours/console/code/src/GOpenGLUi.h
@@ -16,26 +16,6 @@ public:
 private:
     const unsigned int SCR_WIDTH = 400;
     const unsigned int SCR_HEIGHT = 400;
-
-private:
-    const char *vertexShaderSource =""
-        "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
-        "layout (location = 1) in vec3 aColor;\n"
-        "out vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "   gl_Position = vec4(aPos.x, -aPos.y, aPos.z, 1.0);\n"
-        "   ourColor = aColor;\n"
-        "}\0";
-    const char *fragmentShaderSource = ""
-        "#version 330 core\n"
-        "out vec4 FragColor;\n"
-        "in vec3 ourColor;\n"
-        "void main()\n"
-        "{\n"
-        "   FragColor = vec4(ourColor, 1.0f);\n"
-        "}\n\0";
 };
 //==============================================
 #endif
