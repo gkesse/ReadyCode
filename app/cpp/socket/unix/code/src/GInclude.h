44ec9dfa0c9cbbfcbfa871f4fd3f4ac4cc12d17e
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-30 01:11:48 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GInclude.h
+++ app/cpp/socket/unix/code/src/GInclude.h
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <strings.h>
 //==============================================
 #endif
 //==============================================
