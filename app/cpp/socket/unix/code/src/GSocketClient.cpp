b3555c1ccb2a5cb92e3f00e82142fe22c8707b0f
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-30 01:37:22 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GSocketClient.cpp
+++ app/cpp/socket/unix/code/src/GSocketClient.cpp
@@ -11,7 +11,23 @@ GSocketClient::~GSocketClient() {
 }
 //===============================================
 void GSocketClient::run(int argc, char** argv) {
+	int lPort = 8585;
+	int returnStatus = 0;
+	char lBuffer[256] = "";
 
+	int lSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 
+	struct sockaddr_in lAddress;
+	bzero(&lAddress, sizeof(lAddress));
+	lAddress.sin_family = AF_INET;
+	inet_addr(argv[2], &lAddress.sin_addr.s_addr);
+	lAddress.sin_port = htons(lPort);
+
+	connect(lSocket, (struct sockaddr*)&lAddress, sizeof(lAddress));
+
+	read(lSocket, lBuffer, sizeof(lBuffer));
+	std::cout << lBuffer << "...\n";
+
+	close(lSocket);
 }
 //===============================================
