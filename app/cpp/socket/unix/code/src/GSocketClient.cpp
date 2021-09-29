9a7b707c33cfb1d1a749325dea6ee990c9edd2d0
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-30 01:41:06 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GSocketClient.cpp
+++ app/cpp/socket/unix/code/src/GSocketClient.cpp
@@ -11,22 +11,18 @@ GSocketClient::~GSocketClient() {
 }
 //===============================================
 void GSocketClient::run(int argc, char** argv) {
-	int lPort = 8585;
-	char lBuffer[256] = "";
-
+	char lBuffer[256];
 	int lSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
 	struct sockaddr_in lAddress;
 	bzero(&lAddress, sizeof(lAddress));
 	lAddress.sin_family = AF_INET;
-	inet_addr(argv[2], &lAddress.sin_addr.s_addr);
-	lAddress.sin_port = htons(lPort);
-
+	inet_addr("127.0.0.1", &lAddress.sin_addr.s_addr);
+	lAddress.sin_port = htons(8585);
 	connect(lSocket, (struct sockaddr*)&lAddress, sizeof(lAddress));
-
+	//===============================================
 	read(lSocket, lBuffer, sizeof(lBuffer));
 	std::cout << lBuffer << "...\n";
-
+	//===============================================
 	close(lSocket);
 }
 //===============================================
--- app/cpp/socket/unix/code/src/GSocketServer.cpp
+++ app/cpp/socket/unix/code/src/GSocketServer.cpp
@@ -13,25 +13,19 @@ GSocketServer::~GSocketServer() {
 }
 //===============================================
 void GSocketServer::run(int argc, char** argv) {
-	int simplePort = 8585;
-
 	int lSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
 	struct sockaddr_in lAddress;
 	bzero(&lAddress, sizeof(lSocket));
 	lAddress.sin_family = AF_INET;
 	lAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-	lAddress.sin_port = htons(simplePort);
-
+	lAddress.sin_port = htons(8585);
 	bind(lSocket, (struct sockaddr*)&lAddress, sizeof(lAddress));
 	listen(lSocket, 5);
 	//===============================================
 	while (1) {
 		struct sockaddr_in lAddress2 = {0};
 		int lAdresseSize2 = sizeof(lAddress2);
-
 		int lSocket2 = accept(lSocket,(struct sockaddr *)&lAddress2, (socklen_t*)&lAdresseSize2);
-
 		write(lSocket2, APRESSMESSAGE, strlen(APRESSMESSAGE));
 		close(lSocket2);
 	}
