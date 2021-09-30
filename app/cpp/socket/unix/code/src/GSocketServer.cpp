63a336871382ce5a1ec5e8539d53cf96ed67ea87
Gerard KESSE <tiakagerard@hotmail.com>
2021-09-30 08:13:24 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GSocketServer.cpp
+++ app/cpp/socket/unix/code/src/GSocketServer.cpp
@@ -13,24 +13,122 @@ GSocketServer::~GSocketServer() {
 }
 //===============================================
 void GSocketServer::run(int argc, char** argv) {
-    int lSocket = socket(AF_INET, SOCK_DGRAM, 0);
-    struct sockaddr_in lAddress;
-    bzero(&lAddress, sizeof(lAddress));
-    lAddress.sin_family = AF_INET;
-    lAddress.sin_addr.s_addr = htonl(INADDR_ANY);
-    lAddress.sin_port = htons(8585);
-    bind(lSocket, (struct sockaddr*)&lAddress, sizeof(lAddress));
-    //===============================================
-    struct sockaddr_in lAddress2;
-    socklen_t lAdresseSize2 = sizeof(lAddress2);
-    char lBuffer[256];
-    while (1) {
-        recvfrom(lSocket, lBuffer, 256, 0, (struct sockaddr*)&lAddress2, &lAdresseSize2);
-        printf("Received: %s\n", lBuffer);
-        strcpy(lBuffer, "OK");
-        sendto(lSocket, lBuffer, strlen(lBuffer), 0,(struct sockaddr*)&lAddress2, sizeof(lAddress2));
-    }
-    //===============================================
-    close(lSocket);
+	int socket1,socket2;
+	    int addrlen;
+	    struct sockaddr_in xferServer, xferClient;
+	    int returnStatus;
+
+	    /* create a socket */
+	    socket1 = socket(AF_INET, SOCK_STREAM, 0);
+
+	    if (socket1 == -1)
+	    {
+	        fprintf(stderr, "Could not create socket!\n");
+	        exit(1);
+	    }
+
+	    /* bind to a socket, use INADDR_ANY for all local addresses */
+	    xferServer.sin_family = AF_INET;
+	    xferServer.sin_addr.s_addr = INADDR_ANY;
+	    xferServer.sin_port = htons(SERVERPORT);
+
+	    returnStatus = bind(socket1, (struct sockaddr*)&xferServer, sizeof(xferServer));
+
+	    if (returnStatus == -1)
+	    {
+	        fprintf(stderr, "Could not bind to socket!\n");
+	        exit(1);
+	    }
+
+	    returnStatus = listen(socket1, 5);
+
+	    if (returnStatus == -1)
+	    {
+	        fprintf(stderr, "Could not listen on socket!\n");
+	        exit(1);
+	    }
+
+	    for(;;)
+	    {
+
+	        int fd;
+	        int i, readCounter, writeCounter;
+	        char* bufptr;
+	        char buf[MAXBUF];
+	        char filename[MAXBUF];
+
+	        /* wait for an incoming connection */
+	        addrlen = sizeof(xferClient);
+
+	        /* use accept() to handle incoming connection requests        */
+	        /* and free up the original socket for other requests         */
+	        socket2 = accept(socket1, (struct sockaddr*)&xferClient, &addrlen);
+
+	        if (socket2 == -1)
+	        {
+	            fprintf(stderr, "Could not accept connection!\n");
+	            exit(1);
+	        }
+
+	        /* get the filename from the client over the socket */
+	        i = 0;
+
+	        if ((readCounter = read(socket2, filename + i, MAXBUF)) > 0)
+	        {
+	            i += readCounter;
+	        }
+
+	        if (readCounter == -1)
+	        {
+	            fprintf(stderr, "Could not read filename from socket!\n");
+	            close(socket2);
+	            continue;
+	        }
+
+	        filename[i+1] = '\0';
+
+	        printf("Reading file %s\n", filename);
+
+	        /* open the file for reading */
+	        fd = open(filename, O_RDONLY);
+
+	        if (fd == -1)
+	        {
+	            fprintf(stderr, "Could not open file for reading!\n");
+	            close(socket2);
+	            continue;
+	        }
+
+	        /* reset the read counter */
+	        readCounter = 0;
+
+	        /* read the file, and send it to the client in chunks of size MAXBUF */
+	        while((readCounter = read(fd, buf, MAXBUF)) > 0)
+	        {
+	            writeCounter = 0;
+	            bufptr = buf;
+
+	            while (writeCounter < readCounter)
+	            {
+
+	                readCounter -= writeCounter;
+	                bufptr += writeCounter;
+	                writeCounter = write(socket2, bufptr, readCounter);
+
+	                if (writeCounter == -1)
+	                {
+	                    fprintf(stderr, "Could not write file to client!\n");
+	                    close(socket2);
+	                    continue;
+	                }
+	            }
+	        }
+
+	        close(socket2);
+	        close(fd);
+
+	    }
+
+	  close (socket1);
 }
 //===============================================
