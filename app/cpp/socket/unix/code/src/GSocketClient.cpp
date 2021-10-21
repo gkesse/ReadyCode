23bf48cc2f6ce186420084c3f0c90bcaad0c2859
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-21 22:08:18 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GSocketClient.cpp
+++ app/cpp/socket/unix/code/src/GSocketClient.cpp
@@ -14,13 +14,16 @@ GSocketClient::~GSocketClient() {
 //===============================================
 void GSocketClient::run(int argc, char** argv) {
     GSocket lClient;
+    GSocket lServer;
     lClient.ip("127.0.0.1");
     lClient.address();
     lClient.sockets2();
     lClient.binds();
-    lClient.sends(lClient, "Bonjour tout le monde");
-    lClient.recvs(lClient);
+    lClient.sends(lServer, "Bonjour tout le monde");
+    lClient.recvs(lServer);
     lClient.print();
+    lServer.ip();
+    lServer.print2();
     lClient.closes();
 }
 //===============================================
