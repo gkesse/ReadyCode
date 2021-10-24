c2215cb87889ab1c7fa9c25590dca57c6e85b926
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-24 11:52:12 +0200

    Initial Commit

--- app/cpp/socket/unix/unix/envs.sh
+++ app/cpp/socket/unix/unix/envs.sh
@@ -1,4 +1,6 @@
 #================================================
+export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH
+#================================================
 export "GPROJECT_ROOT=/home/rpi4/Programs"
 export "GPROJECT_PATH=${GPROJECT_ROOT}/ReadyCode"
 #================================================
