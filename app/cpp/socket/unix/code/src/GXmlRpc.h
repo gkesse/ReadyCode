0294f6e6853a00f2e421c34fec312cba73687779
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-24 11:14:30 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GXmlRpc.h
+++ app/cpp/socket/unix/code/src/GXmlRpc.h
@@ -12,7 +12,7 @@ public:
     void method(xmlrpc_c::method* _method);
     
 protected:
-    std::shared_ptr<xmlrpc_c::registry> m_registry;
+    xmlrpc_c::registry m_registry;
     std::shared_ptr<xmlrpc_c::method> m_method;
 };
 //==============================================
