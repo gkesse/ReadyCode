c405438ee825a282a50d649835e41d90f513e478
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-27 23:34:09 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GOpenGL.cpp
+++ app/cpp/socket/unix/code/src/GOpenGL.cpp
@@ -82,7 +82,7 @@ void GOpenGL::init2() {
 void GOpenGL::init3() {
 	glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	m_window = glfwCreateWindow(m_width, m_height, m_title.c_str(), NULL, NULL);
 	glfwMakeContextCurrent(m_window);
