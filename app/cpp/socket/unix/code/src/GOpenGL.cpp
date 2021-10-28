63a55cf3cf5f078fde3a0c5a8d2121914e2e9bd5
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-29 01:52:25 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GOpenGL.cpp
+++ app/cpp/socket/unix/code/src/GOpenGL.cpp
@@ -176,7 +176,7 @@ void GOpenGL::shader3(const std::string& _shaderFile, GLenum _shaderType) {
 	GLint lStatus;
 
     lFile.filename(_shaderFile);
-    const GLchar* lShaderCode = lFile.read().c_str();
+    const char* lShaderCode = lFile.read().c_str();
 	m_programID = glCreateShaderProgramv(_shaderType, 1, &lShaderCode);
 	glGetProgramiv(m_programID, GL_LINK_STATUS, &lStatus);
 }
