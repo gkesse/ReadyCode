315851a5473a0d3fcfc1da94d7284028c19fdfb7
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-30 03:01:22 +0200

    Initial Commit

--- app/cpp/socket/unix/code/src/GOpenGLUi.cpp
+++ app/cpp/socket/unix/code/src/GOpenGLUi.cpp
@@ -71,8 +71,8 @@ void GOpenGLUi::run(int argc, char** argv) {
 
 	GLfloat lOuterColor[] = {0.0f, 0.0f, 0.0f, 0.0f};
 	GLfloat lInnerColor[] = {1.0f, 1.0f, 0.75f, 1.0f};
-	GLfloat lInnerRadius = {0.25f};
-	GLfloat lOuterRadius = {0.45f};
+	GLfloat lRadiusInner[] = {0.25f};
+	GLfloat lRadiusOuter[] = {0.45f};
 
 	lOpenGL.uniformBloc("BlobSettings");
 	lOpenGL.uniformBloc(4, lBlobSettings);
