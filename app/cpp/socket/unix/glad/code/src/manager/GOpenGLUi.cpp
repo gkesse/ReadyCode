39753d3164dfdf30e50556989a05fb48c9bbb84f
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-31 13:12:59 +0100

    Initial Commit

--- app/cpp/socket/unix/glad/code/src/manager/GOpenGLUi.cpp
+++ app/cpp/socket/unix/glad/code/src/manager/GOpenGLUi.cpp
@@ -66,7 +66,7 @@ void GOpenGLUi::run(int argc, char** argv) {
 //===============================================
 void GOpenGLUi::onResize(GLFWwindow* _window, int _width, int _height) {
     glViewport(0, 0, _width,_height);
-    lParams.mvp2.projection = glm::perspective(glm::radians(70.0f), (float)_width/_height, 0.3f, 100.0f);
+    lParams.mvp2.projection.perspective(70.0f, _width, _height, 0.3f, 100.0f);
 }
 //===============================================
 void GOpenGLUi::onKey(GLFWwindow* _window, int _key, int _scancode, int _action, int _mods) {
