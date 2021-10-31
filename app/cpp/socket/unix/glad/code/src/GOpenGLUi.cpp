907496c3ffd2f8998b5d4e97de3ae326c2507f04
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-31 02:55:40 +0100

    Initial Commit

--- app/cpp/socket/unix/glad/code/src/GOpenGLUi.cpp
+++ app/cpp/socket/unix/glad/code/src/GOpenGLUi.cpp
@@ -22,6 +22,12 @@ void GOpenGLUi::run(int argc, char** argv) {
     sGApp* lApp = GManager::Instance()->data()->app;
 
     lOpenGL.init(4, 5, 4);
+    lOpenGL.depthOn();
+    lOpenGL.onResize(onResize);
+    lOpenGL.onKey(onKey);
+
+    lOpenGL.shader2(lApp->shader_vertex_file, lApp->shader_fragment_file);
+    lOpenGL.use();
 
     lParams.bgcolor = {0.1f, 0.2f, 0.3f, 1.f};
 
@@ -36,5 +42,26 @@ void GOpenGLUi::run(int argc, char** argv) {
 
     lOpenGL.debug2();
     lOpenGL.close();
+}
+//===============================================
+void GOpenGLUi::onResize(GLFWwindow* _window, int _width, int _height) {
+    glViewport(0,0,_width,_height);
+    lParams.mvp.projection = glm::perspective(glm::radians(70.0f), (float)_width/_height, 0.3f, 100.0f);
+}
+//===============================================
+void GOpenGLUi::onKey(GLFWwindow* _window, int _key, int _scancode, int _action, int _mods) {
+    lOpenGL.onKey(lParams.animate);
+}
+//===============================================
+void GOpenGLUi::onScroll(GLFWwindow* _window, double _x, double _y) {
+
+}
+//===============================================
+void GOpenGLUi::onMouse(GLFWwindow* _window, int _button, int _action, int _mods) {
+
+}
+//===============================================
+void GOpenGLUi::onCursor(GLFWwindow* _window, double _x, double _y) {
+
 }
 //===============================================
