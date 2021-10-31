8c24d19ed689ffca2d991ad33be710d715ef22b6
Gerard KESSE <tiakagerard@hotmail.com>
2021-10-31 02:16:49 +0100

    Initial Commit

--- app/cpp/socket/unix/glad/code/src/GOpenGL.h
+++ app/cpp/socket/unix/glad/code/src/GOpenGL.h
@@ -3,13 +3,18 @@
 #define _GOpenGL_
 //===============================================
 #include "GInclude.h"
+#include "GStruct.h"
+#include "GFunction.h"
 //===============================================
 class GOpenGL {
 public:
 	GOpenGL();
 	~GOpenGL();
 	void init(int _major, int _minor, int _samples);
+	void heatMap(GFunction& _func, float _psize);
+	void heatMap2(GFunction& _func, float _psize);
 	void heatMap(float _v, float _vmin, float _vmax, float& _r, float& _g, float& _b);
+	sGColor heatMap(float _v, float _vmin, float _vmax, const sGColor& _color);
 
 private:
 	GLFWwindow* m_window;
