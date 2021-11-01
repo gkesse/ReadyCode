0405db0b21a364a7489277840c65d76c310f8cb7
Gerard KESSE <tiakagerard@hotmail.com>
2021-11-01 02:46:58 +0100

    Initial Commit

--- app/cpp/socket/unix/glad/code/src/manager/GOpenGLUi.cpp
+++ app/cpp/socket/unix/glad/code/src/manager/GOpenGLUi.cpp
@@ -47,7 +47,7 @@ void GOpenGLUi::run(int argc, char** argv) {
     lOpenGL.uniform("Gravity", glm::vec3(0.0f, -0.2f, 0.0f));
 
     while(!lOpenGL.isClose()) {
-        lOpenGL.bgcolor2(lParams.bgcolor);
+        lOpenGL.bgcolor(lParams.bgcolor);
 
         lOpenGL.times(lParams.times);
         lOpenGL.uniform("Time", lParams.times);
@@ -66,8 +66,8 @@ void GOpenGLUi::run(int argc, char** argv) {
 }
 //===============================================
 void GOpenGLUi::onResize(GLFWwindow* _window, int _width, int _height) {
-    lOpenGL.viewport(_width,_height);
-    lParams.mvp2.projection.perspective(60.0f, 0.3f, 100.0f, _width, _height);
+    lOpenGL.viewport(_width, _height);
+    lParams.mvp2.projection.perspective(70.0f, 0.3f, 100.0f, _width, _height);
 }
 //===============================================
 void GOpenGLUi::onKey(GLFWwindow* _window, int _key, int _scancode, int _action, int _mods) {
