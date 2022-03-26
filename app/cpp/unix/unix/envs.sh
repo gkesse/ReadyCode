8203a1912848b972b5382878f312c845c674f4d4
Gerard KESSE <tiakagerard@hotmail.com>
2022-03-26 14:58:01 +0100

    initial commit

--- app/cpp/unix/unix/envs.sh
+++ app/cpp/unix/unix/envs.sh
@@ -1,7 +1,9 @@
 #================================================
 export "GPROJECT_ROOT=/home/gkesse/Programs"
 export "GPROJECT_PATH=${GPROJECT_ROOT}/ReadyCode"
-export "GPROJECT_SRC=${GPROJECT_PATH}/app/cpp/unix/code/src"
+export "GPROJECT_UNIX=${GPROJECT_PATH}/app/cpp/unix"
+export "GPROJECT_BUILD=${GPROJECT_UNIX}/unix"
+export "GPROJECT_SRC=${GPROJECT_UNIX}/code/src"
 export "GPROJECT_DATA=${GPROJECT_SRC}/data"
 export "GPROJECT_TMP=${GPROJECT_DATA}/tmp"
 export "GPROJECT_SCRIPTS=${GPROJECT_DATA}/scripts"
