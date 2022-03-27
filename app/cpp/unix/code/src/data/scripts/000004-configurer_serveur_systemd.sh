9553a0abdb8431a649c8587aeb12e1b1025c708e
Gerard KESSE <tiakagerard@hotmail.com>
2022-03-27 14:28:34 +0200

    initial commit

--- app/cpp/unix/code/src/data/scripts/000004-configurer_serveur_systemd.sh
+++ app/cpp/unix/code/src/data/scripts/000004-configurer_serveur_systemd.sh
@@ -1,13 +1,7 @@
 #================================================
 # configurer_serveur_systemd
 #================================================
-echo "debut du demarrage du serveur.............: OK"
 #
-export "GPROJECT_DATA=/home/gkesse/Programs/ReadyCode/app/cpp/unix/code/src/data"
-export "GPROJECT_SERVER=${GPROJECT_DATA}/server"
-export "GPROJECT_ENV=PROD"
+cat /etc/systemd/system/pad.service
 #
-cd ${GPROJECT_SERVER}
-./server test socket/server/start
-#
-echo "fin du demarrage du serveur...............: OK"
+
