d42343c48bac2c6ab1a41e4a32f3c651f332608a
Gerard KESSE <tiakagerard@hotmail.com>
2022-03-27 14:37:22 +0200

    initial commit

--- app/cpp/unix/code/src/data/scripts/000004-configurer_serveur_systemd.sh
+++ app/cpp/unix/code/src/data/scripts/000004-configurer_serveur_systemd.sh
@@ -5,4 +5,25 @@
 echo "=====>"
 cat /etc/systemd/system/pad.service
 #
+echo "=====>"
+cat << _EOF_ > /etc/systemd/system/pad.service
+[Unit]
+Description=pad service
+After=network.target
+StartLimitIntervalSec=0
 
+[Service]
+EnvironmentFile=/home/gkesse/Programs/ReadyCode/app/cpp/unix/code/src/data/systemd/server.conf
+Type=simple
+Restart=always
+RestartSec=1
+User=gkesse
+ExecStart=/home/gkesse/Programs/ReadyCode/app/cpp/unix/code/src/data/server/server test socket/server/start
+
+[Install]
+WantedBy=multi-user.target
+_EOF_
+#
+echo "=====>"
+cat /etc/systemd/system/pad.service
+#
