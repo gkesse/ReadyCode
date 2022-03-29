cbf7df029bfbe8df66d84fdc837f04b3ed239c61
Gerard KESSE <tiakagerard@hotmail.com>
2022-03-29 09:08:36 +0200

    initial commit

--- app/unix/000009-creer_service_linux.sh
+++
@@ -1,5 +0,0 @@
-#================================================
-# operations_services_linux
-#================================================
-
-https://medium.com/@benmorel/creating-a-linux-service-with-systemd-611b5c8b91d6
