8a32af34d3b11eea15854ac3862c452cd8e44f6f
Gerard KESSE <tiakagerard@hotmail.com>
2022-03-29 08:10:25 +0200

    initial commit

--- app/cpp/unix/code/src/data/scripts/000002-demarrer_serveur.sh
+++ app/cpp/unix/code/src/data/scripts/000002-demarrer_serveur.sh
@@ -3,7 +3,12 @@
 #================================================
 echo "debut demarrage du serveur................: OK"
 #
-export "GPROJECT_DATA=/home/gkesse/Programs/ReadyCode/app/cpp/unix/code/src/data"
+export "GPROJECT_ROOT=/home/gkesse/Programs"
+export "GPROJECT_PATH=${GPROJECT_ROOT}/ReadyCode"
+export "GPROJECT_UNIX=${GPROJECT_PATH}/app/cpp/unix"
+export "GPROJECT_BUILD=${GPROJECT_UNIX}/unix"
+export "GPROJECT_SRC=${GPROJECT_UNIX}/code/src"
+export "GPROJECT_DATA=${GPROJECT_SRC}/data"
 export "GPROJECT_TMP=${GPROJECT_DATA}/tmp"
 export "GPROJECT_SCRIPTS=${GPROJECT_DATA}/scripts"
 export "GPROJECT_SERVER=${GPROJECT_DATA}/server"
