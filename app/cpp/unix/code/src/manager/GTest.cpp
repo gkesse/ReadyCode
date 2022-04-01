972a2c4cff4240eb06fbc1c781632d8fded7b96a
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-01 13:37:18 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GTest.cpp
+++ app/cpp/unix/code/src/manager/GTest.cpp
@@ -168,8 +168,8 @@ void GTest::runTest(int _argc, char** _argv) {
 
     sprintf(lBuffer, "%*d", BUFFER_SIZE, lInt);
 
-    GLOGW(eGINF, "int.....: [%d]\n", lInt);
-    GLOGW(eGINF, "string..: [%s]\n", lBuffer);
+    GLOGW(eGINF, "int.....: [%d]", lInt);
+    GLOGW(eGINF, "string..: [%s]", lBuffer);
 }
 //===============================================
 void GTest::runIntString(int _argc, char** _argv) {
