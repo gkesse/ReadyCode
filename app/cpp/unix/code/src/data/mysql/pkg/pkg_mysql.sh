bed7b1ccab64783d023e48fe08eb79b802a195d0
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-15 00:55:22 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/pkg/pkg_mysql.sh
+++ app/cpp/unix/code/src/data/mysql/pkg/pkg_mysql.sh
@@ -4,7 +4,7 @@
 GCONFIG=$GCONF/pkg_config.cnf
 #================================================
 ps_sql() {
-    mysql --defaults-extra-file=$GCONFIG -Bse "$2"
+    mysql --defaults-extra-file=$GCONFIG -Bse "$1"
 }
 #================================================
 ps_sql_db() {
