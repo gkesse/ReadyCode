ea20f0f7cc59095560fc819f89fc97a752b998e2
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-15 01:43:37 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
+++ app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
@@ -6,7 +6,7 @@
 #================================================
 ps_sql_db $1 "
 --
-create table if not exists maj (
+create table if not exists maj2 (
     _id int not null,
     _code varchar(6) not null,
     _filename varchar(255) not null,
