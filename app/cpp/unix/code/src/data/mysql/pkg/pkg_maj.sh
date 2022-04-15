2e6a00fc027900dbba4daf3de72adbd399e285e6
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-15 02:33:39 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
+++ app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
@@ -4,16 +4,7 @@
 . $GPROJECT_DATA/mysql/pkg/pkg_var.sh
 . $GPKG/pkg_mysql.sh
 #================================================
-ps_sql_db "$1" "
-create table if not exists maj2 (
-    _id int not null,
-    _code varchar(6) not null,
-    _filename varchar(255) not null,
-    _c_date datetime default current_timestamp,
-    _u_date datetime on update current_timestamp,
-    primary key (_id),
-    unique (_code)
-);
+ps_sql_db "aaaaaaaaaaa" "ooooooooo"
 "
 #================================================
 
