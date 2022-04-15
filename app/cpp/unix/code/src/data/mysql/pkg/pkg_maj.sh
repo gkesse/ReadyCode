0a25095e6ebc350bdd5f56ab79ba10a727f1fb8c
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-15 10:13:23 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
+++ app/cpp/unix/code/src/data/mysql/pkg/pkg_maj.sh
@@ -2,6 +2,7 @@
 #================================================
 . $GPROJECT_DATA/mysql/pkg/pkg_var.sh
 . $GPKG/pkg_mysql.sh
+. $GPKG/pkg_database.sh
 #================================================
 ps_sql_db $1 "
 --
