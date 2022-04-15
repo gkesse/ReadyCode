55056acbc6c84826c4765078b3e32771715ae997
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-15 10:16:32 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/pkg/pkg_database.sh
+++ app/cpp/unix/code/src/data/mysql/pkg/pkg_database.sh
@@ -4,10 +4,9 @@
 . $GPKG/pkg_mysql.sh
 #================================================
 ps_sql "
+--
 create database if not exists pad;
 create database if not exists dev;
-create database if not exists dev01;
-create database if not exists dev02;
-create database if not exists dev03;
+--
 "
 #================================================
