d65a083fdff2abcae1a572f26825eb5a59be9358
Gerard KESSE <tiakagerard@hotmail.com>
2022-04-18 11:04:17 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GXml.cpp
+++ app/cpp/unix/code/src/manager/GXml.cpp
@@ -82,10 +82,6 @@ void GXml::loadXml(const std::string& _data, bool _isFile) {
             return;
         }
     }
-    if(!m_doc) {
-        GERROR_OBJ(eGERR, "Erreur le format XML est invalide.");
-        return;
-    }
     createXPath();
 }
 //===============================================
