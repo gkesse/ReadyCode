92325d61c893152f243f1def9d018e3c5eef7c5c
Gerard KESSE <tiakagerard@hotmail.com>
2022-05-07 15:41:38 +0200

    initial commit

--- app/cpp/unix/code/src/data/mysql/maj/000001-creation_table_user.sh
+++ app/cpp/unix/code/src/data/mysql/maj/000001-creation_table_user.sh
@@ -8,6 +8,8 @@ create table if not exists user (
     _id int not null auto_increment,
     _pseudo varchar(255) not null,
     _password varchar(255) not null,
+    _group char(1) default '2',
+    _active char(1) default '1',
     _c_date datetime default current_timestamp,
     _u_date datetime on update current_timestamp,
     primary key (_id),
