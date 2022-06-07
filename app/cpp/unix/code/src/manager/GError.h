52c94f5b322fc1645af53f8ec7e179811c7955c8
Gerard KESSE <tiakagerard@hotmail.com>
2022-06-07 22:17:20 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GError.h
+++ app/cpp/unix/code/src/manager/GError.h
@@ -9,7 +9,7 @@ public:
     GError();
     ~GError();
     //
-    std::string deserialize(const std::string& _code = "errors");
+    std::string deserialize(const std::string& _code = "errors") const;
     //
     void addError(const char* _name, int _level, const char* _file, int _line, const char* _func, const std::string& _error);
     void addError(const char* _name, int _level, const char* _file, int _line, const char* _func, GError& _error);
