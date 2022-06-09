7357e32c4910d502de1204c3de864ad9100ab590
Gerard KESSE <tiakagerard@hotmail.com>
2022-06-09 22:13:53 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GCode.cpp
+++ app/cpp/unix/code/src/manager/GCode.cpp
@@ -109,7 +109,7 @@ bool GCode::addData(const std::string& _code, const std::vector<std::string>& _d
     createXNode("map");
     for(int i = 0; i < (int)_datas.size(); i++) {
         std::string lData = _datas[i];
-        createCXNode("data");
+        createCRNode("data");
         setNodeValue(lData, _isCData);
     }
     return true;
