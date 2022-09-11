15172e4b618c9e325a83a4a8801004b72a35dcd7
Gerard KESSE <tiakagerard@hotmail.com>
2022-09-11 02:13:12 +0200

    initial commit

--- app/cpp/qmake/mingw/mingw/envs.bat
+++ app/cpp/qmake/mingw/mingw/envs.bat
@@ -1,6 +1,6 @@
 @echo off
 ::===============================================
-set "GPROJECT_ROOT=C:\Users\tiaka\Downloads\ePrograms"
+set "GPROJECT_ROOT=C:\Users\tiaka\Downloads\eTools\ePrograms"
 ::===============================================
 set "PATH=C:\Qt\6.3.1\mingw_64\bin;%PATH%"
 set "PATH=C:\Qt\Tools\mingw1120_64\bin;%PATH%"
