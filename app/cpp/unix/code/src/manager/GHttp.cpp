decf4d0f6a729d3ee59a19ab2498c424a93e3a28
Gerard KESSE <tiakagerard@hotmail.com>
2022-09-15 08:01:03 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GHttp.cpp
+++ app/cpp/unix/code/src/manager/GHttp.cpp
@@ -17,15 +17,15 @@ void GHttp::setMethod(const GString2& _method) {
     m_method = _method;
 }
 //===============================================
-void GHttp::setUrl(const std::string& _url) {
+void GHttp::setUrl(const GString2& _url) {
     m_url = _url;
 }
 //===============================================
-void GHttp::setVersion(const std::string& _version) {
+void GHttp::setVersion(const GString2& _version) {
     m_version = _version;
 }
 //===============================================
-void GHttp::setHostname(const std::string& _hostname) {
+void GHttp::setHostname(const GString2& _hostname) {
     m_hostname =_hostname;
 }
 //===============================================
@@ -33,15 +33,15 @@ void GHttp::setPort(int _port) {
     m_port = _port;
 }
 //===============================================
-void GHttp::setStatus(const std::string& _status) {
+void GHttp::setStatus(const GString2& _status) {
     m_status = _status;
 }
 //===============================================
-void GHttp::setReason(const std::string& _reason) {
+void GHttp::setReason(const GString2& _reason) {
     m_reason = _reason;
 }
 //===============================================
-void GHttp::setContentType(const std::string& _contentType) {
+void GHttp::setContentType(const GString2& _contentType) {
     m_contentType = _contentType;
 }
 //===============================================
@@ -49,51 +49,51 @@ void GHttp::setContentLength(int _contentLength) {
     m_contentLength = _contentLength;
 }
 //===============================================
-void GHttp::setCacheControl(const std::string& _cacheControl) {
+void GHttp::setCacheControl(const GString2& _cacheControl) {
     m_cacheControl = _cacheControl;
 }
 //===============================================
-void GHttp::setUpgradeInsecureRequests(const std::string& _upgradeInsecureRequests) {
+void GHttp::setUpgradeInsecureRequests(const GString2& _upgradeInsecureRequests) {
     m_upgradeInsecureRequests = _upgradeInsecureRequests;
 }
 //===============================================
-void GHttp::setUserAgent(const std::string& _userAgent) {
+void GHttp::setUserAgent(const GString2& _userAgent) {
     m_userAgent = _userAgent;
 }
 //===============================================
-void GHttp::setAccept(const std::string& _accept) {
+void GHttp::setAccept(const GString2& _accept) {
     m_accept = _accept;
 }
 //===============================================
-void GHttp::setAcceptEncoding(const std::string& _acceptEncoding) {
+void GHttp::setAcceptEncoding(const GString2& _acceptEncoding) {
     m_acceptEncoding = _acceptEncoding;
 }
 //===============================================
-void GHttp::setAcceptLanguage(const std::string& _acceptLanguage) {
+void GHttp::setAcceptLanguage(const GString2& _acceptLanguage) {
     m_acceptLanguage = _acceptLanguage;
 }
 //===============================================
-void GHttp::setDate(const std::string& _date) {
+void GHttp::setDate(const GString2& _date) {
     m_date = _date;
 }
 //===============================================
-void GHttp::setServer(const std::string& _server) {
+void GHttp::setServer(const GString2& _server) {
     m_server = _server;
 }
 //===============================================
-void GHttp::setLastModified(const std::string& _lastModified) {
+void GHttp::setLastModified(const GString2& _lastModified) {
     m_lastModified = _lastModified;
 }
 //===============================================
-void GHttp::setConnection(const std::string& _connection) {
+void GHttp::setConnection(const GString2& _connection) {
     m_connection = _connection;
 }
 //===============================================
-bool GHttp::getRequest(std::string& _request) {
+bool GHttp::getRequest(GString2& _request) {
     return false;
 }
 //===============================================
-bool GHttp::getResponse(std::string& _response) {
+bool GHttp::getResponse(GString2& _response) {
     return false;
 }
 //===============================================
