801698c5dcf7bb2146d965623e0f0923a55df1b9
Gerard KESSE <tiakagerard@hotmail.com>
2022-09-21 23:40:03 +0200

    initial commit

--- app/cpp/unix/code/src/manager/GCode2.cpp
+++ app/cpp/unix/code/src/manager/GCode2.cpp
@@ -65,6 +65,7 @@ bool GCode2::getData(const GString2& _code, std::vector<GObject2*>& _map, GObjec
         lDom.createCode();
         lDom.loadNode(lData);
         lData = lDom.toString();
+        lData.print();
         GObject2* lObj = _obj->clone();
         lObj->deserialize(lData, _code);
         _map.push_back(lObj);
