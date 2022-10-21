a9a9b405ba7f8728438c2b1e98740d6d03cca2d6
Gerard KESSE <tiakagerard@hotmail.com>
2022-10-21 07:48:44 +0200

    mise a jour doc

---
+++ doc/certbot_genere_un_certificat.sh
@@ -0,0 +1,13 @@
+# prerequis
+->  ready.pad.server doit etre du domain public
+
+# procedure
+sudo systemctl stop apache2
+
+sudo certbot certonly --standalone --agree-tos --no-eff-email \
+-d ready.pad.server
+
+sudo systemctl restart apache2
+
+# affichage du log
+sudo cat /var/log/letsencrypt/letsencrypt.log
---
+++ doc/telnet_verifier_l_utilisation_d_un_port.sh
@@ -0,0 +1,2 @@
+telnet localhost 80
+telnet localhost 443
