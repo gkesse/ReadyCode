616453f7573dcc323ea890cc77e831e155729247
Gerard KESSE <tiakagerard@hotmail.com>
2022-11-30 23:30:17 +0100

    gestion du module de configuration

--- app/cpp/unix/code/src/manager/GModuleType.cpp
+++ app/cpp/unix/code/src/manager/GModuleType.cpp
@@ -64,7 +64,7 @@ bool GModuleType::onModule() {
 }
 //===============================================
 bool GModuleType::onSaveModuleType() {
-    if(m_name == "") {GERROR_ADD(eGERR, "Le nom est obligatoire."); return false;}
+    if(m_name == "") {GERROR_ADD(eGERR, "Le nom du type est obligatoire."); return false;}
     if(!saveModuleType()) return false;
     if(m_id == 0) {GERROR_ADD(eGERR, "Erreur lors de l'enregistrement du module."); return false;}
     GLOG_ADD(eGLOG, "Le module a bien été enregistré.");
