15d639a111f4d43ef77b434f30752e4877f6f0c3
Gerard KESSE <tiakagerard@hotmail.com>
2022-12-15 09:06:40 +0100

    gestion du module poco

--- app/cpp/unix/code/src/manager/GPoco.cpp
+++ app/cpp/unix/code/src/manager/GPoco.cpp
@@ -4,47 +4,47 @@
 //===============================================
 GPoco::GPoco(const GString& _code)
 : GObject(_code) {
-
+    m_status = 0;
 }
 //===============================================
 GPoco::~GPoco() {
 
 }
 //===============================================
-bool GPoco::doRequest(Poco::Net::HTTPClientSession& session, Poco::Net::HTTPRequest& request, Poco::Net::HTTPResponse& response) {
-    session.sendRequest(request);
-    std::istream& rs = session.receiveResponse(response);
-    std::cout << response.getStatus() << " " << response.getReason() << std::endl;
-    if (response.getStatus() != Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED) {
-        Poco::StreamCopier::copyStream(rs, std::cout);
-        return true;
-    }
-    else {
-        Poco::NullOutputStream null;
-        Poco::StreamCopier::copyStream(rs, null);
-        return false;
-    }
+void GPoco::setUri(const GString& _uri) {
+    m_uri = _uri;
+}
+//===============================================
+bool GPoco::extractCredentials() {
+    if(m_uri.isEmpty()) return false;
+    Poco::URI lUri(m_uri.c_str());
+    std::string lUsername;
+    std::string lPassword;
+    Poco::Net::HTTPCredentials::extractCredentials(lUri, lUsername, lPassword);
+    Poco::Net::HTTPCredentials credentials(lUsername, lPassword);
+    m_username = lUsername;
+    m_password = lPassword;
+    if(m_username.isEmpty()) return false;
+    if(m_password.isEmpty()) return false;
     return true;
 }
 //===============================================
-bool GPoco::doGet(const GString& _uri) {
+bool GPoco::doGet() {
+    if(m_uri.isEmpty()) return false;
     try {
-        Poco::URI uri(_uri.c_str());
-        GString path(uri.getPathAndQuery());
-        if (path.isEmpty()) path = "/";
+        Poco::URI lUri(m_uri.c_str());
+        GString lPath(lUri.getPathAndQuery());
+        if (lPath.isEmpty()) lPath = "/";
 
-        std::string username;
-        std::string password;
-        Poco::Net::HTTPCredentials::extractCredentials(uri, username, password);
-        Poco::Net::HTTPCredentials credentials(username, password);
+        Poco::Net::HTTPClientSession lSession(lUri.getHost(), lUri.getPort());
+        Poco::Net::HTTPRequest lRequest(Poco::Net::HTTPRequest::HTTP_GET, lPath.c_str(), Poco::Net::HTTPMessage::HTTP_1_1);
+        Poco::Net::HTTPResponse lResponse;
 
-        Poco::Net::HTTPClientSession session(uri.getHost(), uri.getPort());
-        Poco::Net::HTTPRequest request(Poco::Net::HTTPRequest::HTTP_GET, path.c_str(), Poco::Net::HTTPMessage::HTTP_1_1);
-        Poco::Net::HTTPResponse response;
-
-        if (!doRequest(session, request, response)) {
-            credentials.authenticate(request, response);
-            if (!doRequest(session, request, response)) {
+        if (!doRequest(lSession, lRequest, lResponse)) {
+            extractCredentials();
+            Poco::Net::HTTPCredentials lCredentials(m_username.c_str(), m_password.c_str());
+            lCredentials.authenticate(lRequest, lResponse);
+            if (!doRequest(lSession, lRequest, lResponse)) {
                 GERROR_ADD(eGERR, "Erreur lors de l'exécution de la requête.");
                 return false;
             }
@@ -52,6 +52,29 @@ bool GPoco::doGet(const GString& _uri) {
     }
     catch(Poco::Exception& e) {
         GERROR_ADD(eGERR, "Erreur lors de l'exécution de la requête.\n%s", e.displayText().c_str());
+        return false;
+    }
+    return true;
+}
+//===============================================
+bool GPoco::doGet(const GString& _uri) {
+    setUri(_uri);
+    if(!doGet()) return false;
+    return true;
+}
+//===============================================
+bool GPoco::doRequest(Poco::Net::HTTPClientSession& _session, Poco::Net::HTTPRequest& _request, Poco::Net::HTTPResponse& _response) {
+    _session.sendRequest(_request);
+    std::istream& lStream = _session.receiveResponse(_response);
+    m_status = _response.getStatus();
+    m_reason = _response.getReason();
+    if (m_status != Poco::Net::HTTPResponse::HTTP_UNAUTHORIZED) {
+        Poco::StreamCopier::copyStream(lStream, std::cout);
+        return true;
+    }
+    else {
+        Poco::NullOutputStream lNull;
+        Poco::StreamCopier::copyStream(lStream, lNull);
         return false;
     }
     return true;
--- app/cpp/unix/code/src/manager/GPoco.h
+++ app/cpp/unix/code/src/manager/GPoco.h
@@ -8,10 +8,21 @@ class GPoco : public GObject {
 public:
     GPoco(const GString& _code = "poco");
     ~GPoco();
-    bool doRequest(Poco::Net::HTTPClientSession& _session, Poco::Net::HTTPRequest& _request, Poco::Net::HTTPResponse& _response);
+
+    void setUri(const GString& _uri);
+    bool extractCredentials();
+
+    bool doGet();
     bool doGet(const GString& _uri);
 
+    bool doRequest(Poco::Net::HTTPClientSession& _session, Poco::Net::HTTPRequest& _request, Poco::Net::HTTPResponse& _response);
+
 private:
+    GString m_uri;
+    GString m_username;
+    GString m_password;
+    GString m_reason;
+    int m_status;
 };
 //==============================================
 #endif
