f1e952a416bfb5d0658405bd9a218013f247aca6
Gerard KESSE <tiakagerard@hotmail.com>
2022-12-20 19:54:42 +0100

    mise a jour du module socket

--- app/cpp/qmake/mingw/code/src/manager/GSocketEcho.cpp
+++ app/cpp/qmake/mingw/code/src/manager/GSocketEcho.cpp
@@ -12,8 +12,16 @@ GSocketEcho::~GSocketEcho() {
 }
 //===============================================
 bool GSocketEcho::onCallServer() {
+    m_dataIn = GFORMAT("%*d%s", DATA_LENGTH_SIZE, m_dataIn.size(), m_dataIn.c_str());
     sendData(m_dataIn);
-    readData(m_dataOut, 487);
+    GString lHeader;
+    readData(lHeader, DATA_LENGTH_SIZE);
+    int lHeaderSize = lHeader.size();
+    int lDataSize = lHeader.substr(0, DATA_LENGTH_SIZE).toInt();
+    int lTotalSize = lDataSize + DATA_LENGTH_SIZE;
+    int lDiffSize = lTotalSize - lHeaderSize;
+    m_dataOut = lHeader.substr(DATA_LENGTH_SIZE);
+    readData(m_dataOut, lDiffSize);
     return true;
 }
 //===============================================
--- app/cpp/unix/code/src/manager/GSocketEcho.cpp
+++ app/cpp/unix/code/src/manager/GSocketEcho.cpp
@@ -19,8 +19,17 @@ bool GSocketEcho::runThreadCB() {
 }
 //===============================================
 bool GSocketEcho::runRequest() {
-    readData(m_dataIn, 487);
-    m_dataOut = m_dataIn;
+    GString lHeader;
+    readData(lHeader, DATA_LENGTH_SIZE);
+    int lHeaderSize = lHeader.size();
+    int lDataSize = lHeader.substr(0, DATA_LENGTH_SIZE).toInt();
+    int lTotalSize = lDataSize + DATA_LENGTH_SIZE;
+    int lDiffSize = lTotalSize - lHeaderSize;
+    m_dataIn = lHeader.substr(DATA_LENGTH_SIZE);
+    readData(m_dataIn, lDiffSize);
+
+    m_dataIn = GFORMAT("[SERVER] : %s", m_dataIn.c_str());
+    m_dataOut = GFORMAT("%*d%s", DATA_LENGTH_SIZE, m_dataOut.size(), m_dataOut.c_str());
     sendData(m_dataOut);
     return true;
 }
