662a96674619b459e8a6f7bed24b70ee7e89744e
Gerard KESSE <tiakagerard@hotmail.com>
2022-12-27 13:40:41 +0100

    mise a jour du module poco

--- app/cpp/unix/code/src/manager/GPoco.cpp
+++ app/cpp/unix/code/src/manager/GPoco.cpp
@@ -367,7 +367,7 @@ bool GPoco::initSSL() {
             , m_privateKeyFile.c_str()
             , m_certificateFile.c_str()
             , ""
-            , Poco::Net::Context::VERIFY_NONE
+            , Poco::Net::Context::VERIFY_STRICT
             , 9
             , false
             , "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH"
