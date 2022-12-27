e1919ea3496ad39c659eddf46ce0b2f259c4212b
Gerard KESSE <tiakagerard@hotmail.com>
2022-12-27 13:31:31 +0100

    mise a jour du module poco

--- app/cpp/unix/code/src/manager/GPoco.cpp
+++ app/cpp/unix/code/src/manager/GPoco.cpp
@@ -366,7 +366,7 @@ bool GPoco::initSSL() {
             Poco::Net::Context::SERVER_USE
             , m_privateKeyFile.c_str()
             , m_certificateFile.c_str()
-            , m_cacertFile.c_str()
+            , ""
             , Poco::Net::Context::VERIFY_RELAXED
             , 9
             , false
