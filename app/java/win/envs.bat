a147006c8a3b7e17b50902d86af9e5a54a3a2029
Gerard KESSE <tiakagerard@hotmail.com>
2023-06-24 13:04:43 +0200

    tuto java

--- app/java/win/envs.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-::===============================================
-set "PATH="
-set "PATH=C:\Users\tiaka\Downloads\eBin\winlibs\mingw64\bin;%PATH%"
-set "PATH=C:\Program Files\Java\jdk-18.0.2.1\bin;%PATH%"
-::===============================================
