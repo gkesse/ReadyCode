b0b7b22a3e6f9eaa6463bbb6118ffb448ca21a04
Gerard KESSE <tiakagerard@hotmail.com>
2023-08-03 19:07:25 +0200

    module 8051

--- app/embedsys/8051/c/client/code/src/GSchTask.c
+++ app/embedsys/8051/c/client/code/src/GSchTask.c
@@ -3,18 +3,21 @@
 #include "GSch.h"
 #include "GLedFlash.h"
 #include "GChenillard.h"
+#include "G7SegCount.h"
 //===============================================
 static uchar gAction;
 //===============================================
 static void GSchTask_initLedFlash();
 static void GSchTask_initLedChenillard();
+static void GSchTask_init7SegCount();
 //===============================================
 // Public
 //===============================================
 void GSchTask_init() {
-    gAction = 1;
+    gAction = 2;
     if(gAction == 0) GSchTask_initLedFlash();
     else if(gAction == 1) GSchTask_initLedChenillard();
+    else if(gAction == 2) GSchTask_init7SegCount();
 }
 //===============================================
 // LedFlash
@@ -29,5 +32,12 @@ static void GSchTask_initLedFlash() {
 static void GSchTask_initLedChenillard() {
     GChenillard_init();
     GSch_addTask(GChenillard_update, 1, 500);
+}
+//===============================================
+// 7SegCount
+//===============================================
+static void GSchTask_init7SegCount() {
+    G7SegCount_init();
+    GSch_addTask(G7SegCount_update, 1, 500);
 }
 //===============================================
