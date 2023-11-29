5bff6665322a01a10e4730516ae72994c4fb0e69
Gerard KESSE <tiakagerard@hotmail.com>
2023-11-29 08:12:27 +0100

    tuto c

--- app/c/tuto/p00_mingw/cmd/z_compile.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-::===============================================
-set "PATH="
-set "PATH=C:\Users\tiaka\Downloads\eBin\winlibs\mingw64\bin"
-::===============================================
-gcc -c main.c -o main.o
-gcc -o readyapp.exe main.o
-readyapp.exe
-::===============================================
